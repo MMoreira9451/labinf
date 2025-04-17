17dc2ad26248c70e9610c27c7b0d5b0b753e7f9e
MMoreira9451 <mmoreira@alumnos.uai.cl>
2025-04-16 20:05:55 -0400

    Update horas.tsx

--- front-end/app/(tabs)/horas.tsx
+++ front-end/app/(tabs)/horas.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, FlatList, RefreshControl, TouchableOpacity, Platform } from 'react-native';
 
 const API_BASE = Platform.OS === 'web'
-  ? 'http://10.0.5.123:5000'
+  ? 'http://127.0.0.1:5000'
   : 'http://10.0.5.63:8081';
 
 export default function HorasAcumuladasScreen() {
