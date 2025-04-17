d9cd87de05f9d6b82d5cca215816bc9a5d3303fa
MMoreira9451 <mmoreira@alumnos.uai.cl>
2025-04-16 20:56:35 -0400

    Update index.tsx

--- front-end/app/(tabs)/index.tsx
+++ front-end/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import QRCode from 'react-native-qrcode-svg';
 import { StatusBar } from 'expo-status-bar';
 
 const API_BASE = Platform.OS === 'web'
-  ? 'http://127.0.0.1:5000'
+  ? 'http://10.0.5.63:5000'
   : 'http://10.0.5.63:8081'; // Replace if using Expo Go on mobile
 
 export default function QRGenerator() {
