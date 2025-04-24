2bd1c48098b34cffa8fe6c2c058af75121d67eda
Benjamín Muñoz <benjamunoz8@gmail.com>
2025-04-23 20:03:24 -0400

    Arreglada API

--- front-end/app/(tabs)/horas.tsx
+++ front-end/app/(tabs)/horas.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, FlatList, RefreshControl, TouchableOpacity, Platform, ActivityIndicator } from 'react-native';
 
 // Usar una constante directa para la URL de la API
-const API_BASE = 'http://10.0.3.54:5000';
+const API_BASE = 'https://acceso.informaticauaint.com/api';
 
 export default function HorasAcumuladasScreen() {
   const [horasData, setHorasData] = useState([]);
